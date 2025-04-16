562351d93fdf39c6f6dbcf9081954145845ea8d8
Seryoyo <harogio101@gmail.com>
2025-04-16 14:09:12 -0700

    add floatingText, Weapons, scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,10 +29,20 @@ public class GameManager : MonoBehaviour
     //References
     public Player player;
     //public weapon weapon...
+    public floatingTextManager floatingTextManager;
 
     //Logic
     public int pesos;
     public int experience;
+
+
+    //floating text
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+
 
     //Save State
     /*
